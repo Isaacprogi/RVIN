c6de3ab64d2071de6b76c4a1eb8cffa75e7d0485
isaacprogi <isaaconyes80@gmail.com>
2025-06-09 16:07:36 +0100

    fix: resloved swiper type error

--- src/components/SecessPartner/SuccessPartner.tsx
+++ src/components/SecessPartner/SuccessPartner.tsx
@@ -1,6 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
-import 'swiper/css';
 import { successPartners, successPartnersImage } from '../../data/data';
 
 const SuccessPartners = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0.7' },
+        },
+      },
+      animation: {
+        blink: 'blink 0.6s ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
+
